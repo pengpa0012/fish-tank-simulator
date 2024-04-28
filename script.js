096d8b239e2f55d9c8f2e258f905f1739f6f3d10
Pengpa0012 <peng.ilikha@gmail.com>
2024-04-28 15:25:47 +0800

    added food

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
 let fishes = new Array(3)
+let food = new Array(0)
 let randomD, randomX, randomY
-
+let gravityAcceleration = 0.1
 
 class Fish {
   constructor(x, y, d, xspeed, yspeed) {
@@ -23,6 +24,17 @@ class Fish {
   }
 }
 
+class Food {
+  constructor(x, y, d, yspeed) {
+    this.drawFish = function () {
+      circle(x, y, d)
+    }
+    this.drop = function () {
+      y -= yspeed + gravityAcceleration
+    }
+  }
+}
+
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight)
   for(let i = 0; i < fishes.length; i++) {
@@ -35,9 +47,15 @@ function setup() {
 
 function draw() {
   clear()
+  // draw fish
   for (let i = 0; i < fishes.length; i++) {
     fishes[i].drawFish()
     fishes[i].moveFish()
+  }
+  // draw food
+  for (let i = 0; i < food.length; i++) {
+    food[i].drawFish()
+    food[i].drop()
   }
 }
 
@@ -45,12 +63,20 @@ function windowResized() {
   resizeCanvas(window.innerWidth, window.innerHeight)
 }
 
+// add new fish
+// function mouseClicked() {
+//   console.log(mouseX, mouseY)
+//   const randomD = random(10, 100)
+//   const randomX = random(-5, 5)
+//   const randomY = random(-5, 5)
+//   const newFish = new Fish(mouseX, mouseY, randomD, randomX, randomY)
+//   fishes.push(newFish)
+// }
+
+
 function mouseClicked() {
   console.log(mouseX, mouseY)
-  const randomD = random(10, 100)
-  const randomX = random(-5, 5)
-  const randomY = random(-5, 5)
-  const newFish = new Fish(mouseX, mouseY, randomD, randomX, randomY)
-  fishes.push(newFish)
+  const randomY = random(-3, -1)
+  const newFood = new Food(mouseX, mouseY, 10, randomY)
+  food.push(newFood)
 }
-
