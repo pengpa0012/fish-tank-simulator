572536cdc7bf80b61e80a59e74d7f4df6269b2f3
Pengpa0012 <peng.ilikha@gmail.com>
2024-04-28 14:00:03 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const canvas = document.querySelector("canvas")
+const ctx = canvas.getContext("2d")
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+ctx.fillRect(200, 200, 100, 100)
