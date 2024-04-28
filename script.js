d34c392e52059d55cc914d2a2cd5661586dfd9ed
Pengpa0012 <peng.ilikha@gmail.com>
2024-04-28 14:43:10 +0800

    setup p5 js

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
-const canvas = document.querySelector("canvas")
-const ctx = canvas.getContext("2d")
 
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
+let fishes = new Array(40)
+let randomD, randomX, randomY
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight)
+  for(let i = 0; i < fishes.length; i++) {
+    randomD = random(10, 100)
+    randomX = random(-5, 5)
+    randomY = random(-5, 5)
+    fishes[i] = new Fish(width/2, height/2, randomD, randomX, randomY)
+  }
+}
 
-ctx.fillRect(200, 200, 100, 100)
+function draw() {
+  clear()
+  for (let i = 0; i < fishes.length; i++) {
+    fishes[i].drawFish()
+    fishes[i].moveFish()
+  }
+}
+
+function windowResized() {
+  resizeCanvas(window.innerWidth, window.innerHeight)
+}
+
+
+class Fish {
+  constructor(x, y, d, xspeed, yspeed) {
+    this.drawFish = function () {
+      circle(x, y, d)
+    }
+    this.moveFish = function () {
+      x = x + xspeed
+      y = y + yspeed
+      if (x > width - d / 2 || x < d / 2) {
+        xspeed = xspeed * -1
+      }
+
+      if (y > height - d / 2 || y < d / 2) {
+        yspeed = yspeed * -1
+      }
+    }
+  }
+}
